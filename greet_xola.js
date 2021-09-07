319d007123cdb40b94a304fe482509168c724f51
Lesego864 <tshipulesego@gmail.com>
2021-09-08 01:28:39 +0200

    tried to add figlet module

--- greet_xola.js
+++ greet_xola.js
@@ -1,8 +1,21 @@
 // const greet = require('./greet');
 // console.log(greet('Xola'))
 
-const chalk = require('chalk');
-const greet = require('./greet');
+// const chalk = require('chalk');
+// const greet = require('./greet');
 
-const styledMessage = chalk.bgGreen.black(greet('Xola'));
-console.log(styledMessage)
+// const styledMessage = chalk.bgGreen.black(greet('Xola'));
+// console.log(styledMessage);
+
+
+var figlet = require('figlet');
+
+figlet('Hello, Xola', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
+
